bc4e52d579fa58bfb4b2ea0164197180a9c528e9
lynnjepsen <lynnjepsen@google.com>
2018-11-07 19:59:54 -0800

    fix navigator when scrolling

--- src/form/navigator/index.js
+++ src/form/navigator/index.js
@@ -6,10 +6,42 @@ import './index.scss';
 
 class Navigator extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      classes: "navigator "+props.className,
+      top: 0,
+      left: 0
+    }
+    this.element = React.createRef();
+  }
+
+  componentDidMount() {
+    const viewportPosition = this.getViewportPosition();
+    this.setState({
+      top: viewportPosition.y, 
+      left: viewportPosition.x, 
+      classes: this.state.classes+" navigator--fixed"});
+  }
+
+  getViewportPosition() {
+    var xPosition = 0;
+    var yPosition = 0;
+
+    let element = this.element.current;
+    while(element) {
+        xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
+        yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+        element = element.offsetParent;
+    }
+
+    return { x: xPosition, y: yPosition };
+  }
+
   render() {
-    const className = "navigator "+this.props.className;
+    const style = {top:this.state.top, left:this.state.left};
     return (
-      <div className={className}>
+      <div className={this.state.classes} ref={this.element} style={style}>
         <div className="navigator__question-set">
           {this.renderPersonalInformationIndictor()}
           <span className="navigator__question-set-name">Personal Information</span>
