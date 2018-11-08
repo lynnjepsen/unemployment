648c721caa126505635c60a5a6793e94efa32bb3
Alisa Weinstein <amweinstein@amweinstein-macbookpro.roam.corp.google.com>
2018-11-07 16:35:16 -0800

    Added written language text field

--- src/form/personal-information/index.js
+++ src/form/personal-information/index.js
@@ -9,12 +9,13 @@ class PersonalInformation extends Component {
     this.state = {
       isBirthdaySet: false,
       isGenderSet: false,
+      isWrittenLanguageSet: false,
       isComplete: false
     };
   }
 
   componentDidUpdate(prevProps, prevState) {
-    const isComplete = this.state.isBirthdaySet && this.state.isGenderSet;
+    const isComplete = this.state.isBirthdaySet && this.state.isGenderSet && this.state.isWrittenLanguageSet;
     if (isComplete !== prevState.isComplete) {
       this.setState({isComplete:isComplete});
       if (this.props.onComplete) {
@@ -34,6 +35,10 @@ class PersonalInformation extends Component {
           label="Gender"
           question="What is your gender?"
           onUserInput={(isUserInput) => this.setState({isGenderSet: isUserInput})}/>
+           <TextField className="personal-information__written-language"
+          label="Written Language"
+          question="What is your preferred written language?"
+          onUserInput={(isUserInput) => this.setState({isBirthdaySet: isUserInput})}/>
       </div>
     );
   }
